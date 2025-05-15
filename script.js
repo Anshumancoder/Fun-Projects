159b240c54745b07582492705d8b5ddb3b26a3a7
Anshuman Verma <157945418+Anshumancoder@users.noreply.github.com>
2025-05-15 18:16:52 +0530

    adding html and some js

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container")
+const playerturn = document.getElementById("playerturn")
+const startscreen = document.querySelector(".startscreen")
+const startbutton = document.getElementById("start")
+const message = document.getElementById("message")
+
+let initialmatrix = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],  
+    [0, 0, 0, 0, 0, 0, 0],  
+]
+
